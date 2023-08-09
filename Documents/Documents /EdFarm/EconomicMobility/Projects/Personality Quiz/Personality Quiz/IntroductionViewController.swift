486df387588fc91644084fb7ac50bc63ea5a0655
Alandis <1seals.alandis@gmail.com>
2023-08-09 17:15:03 -0500

    Finished setting up UI

---
+++ Documents/Documents /EdFarm/EconomicMobility/Projects/Personality Quiz/Personality Quiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Alandis Seals on 8/9/23.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
